46a2952fa8134ce1875c3fee708fb326b4f75428
susantacemk <cse21083@cemk.ac.in>
2024-11-08 06:18:42 +0000

    create project using two methods

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1> Hello Susanta ! you create a project using npx module</h1>
+  );
+}
+
+export default App;
---
+++ vite_react/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+
+
+
+function App() {
+ return(
+  <h1> hello Susanta ! You are learned React First time</h1>
+ )
+}
+
+export default App
